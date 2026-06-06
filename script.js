df429652496377022c18cbd2421d870e99a24336
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2026-06-06 18:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const incBtn = document.querySelector("#incBtn");
+const counter = document.querySelector("#counter");
+
+incBtn.addEventListener("click", () => {
+	let curr = counter.innerText;
+	let next = Number(curr) + 1;
+	alert(next);
+	counter.innerText = next;
+})
